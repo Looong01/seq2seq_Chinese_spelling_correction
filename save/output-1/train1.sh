20349fe007eb47dc13f9c1dfa0e0a4c0ec589d37
loong <lizelongdd@sina.com>
2022-05-20 14:19:17 +0800

    5

---
+++ save/output-1/train1.sh
@@ -0,0 +1,31 @@
+# source /f2020/yang/anaconda3/bin/activate
+# conda activate PyTorch
+
+# 实验1
+python train.py batch_size-8_dropout-0dot25_epochs-200.py
+python train.py batch_size-16_dropout-0dot25_epochs-200.py
+python train.py batch_size-32_dropout-0dot25_epochs-200.py
+python train.py batch_size-64_dropout-0dot25_epochs-200.py
+python train.py batch_size-128_dropout-0dot25_epochs-200.py
+# python train.py batch_size-256_dropout-0dot25_epochs-200.py
+
+# # 实验2
+# python train.py batch_size-32_dropout-0dot5_epochs-200.py
+# python train.py batch_size-64_dropout-0dot5_epochs-200.py
+# python train.py batch_size-128_dropout-0dot5_epochs-200.py
+# python train.py batch_size-256_dropout-0dot5_epochs-200.py
+
+# # 实验3
+# python train.py batch_size-32_dropout-0dot25_epochs-20000.py
+# python train.py batch_size-64_dropout-0dot25_epochs-20000.py
+# python train.py batch_size-128_dropout-0dot25_epochs-20000.py
+
+# # 实验4
+# python train.py batch_size-32_dropout-0dot3_epochs-20000.py
+# python train.py batch_size-64_dropout-0dot3_epochs-20000.py
+# python train.py batch_size-128_dropout-0dot3_epochs-20000.py
+
+# # 实验5
+# python train.py batch_size-32_dropout-0dot2_epochs-20000.py
+# python train.py batch_size-64_dropout-0dot2_epochs-20000.py
+# python train.py batch_size-128_dropout-0dot2_epochs-20000.py
---
+++ train2.sh
@@ -0,0 +1,7 @@
+python train.py batch_size-64_dropout-0dot3_epochs-5000.py
+python train.py batch_size-128_dropout-0dot3_epochs-5000.py
+
+# 实验5
+python train.py batch_size-32_dropout-0dot2_epochs-5000.py
+python train.py batch_size-64_dropout-0dot2_epochs-5000.py
+python train.py batch_size-128_dropout-0dot2_epochs-5000.py

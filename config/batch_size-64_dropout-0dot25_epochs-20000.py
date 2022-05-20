3a3c8082b187d1907c40bfabd49ae1262a7bae32
loong <lizelongdd@sina.com>
2022-05-20 11:58:56 +0800

    4

--- config/batch_size-64_dropout-0dot25_epochs-20000.py
+++ config/batch_size-64_dropout-0dot25_epochs-20000.py
@@ -17,7 +17,9 @@ pwd_path = os.path.abspath(os.path.dirname(__file__) + '/../')
 
 data_dir = os.path.join(pwd_path, 'data')
 
-output_dir = os.path.join(pwd_path, 'output/batch_size-{}_dropout-{}_epochs-{}'.format(batch_size, dropout, epochs))
+trainlogs_path = os.path.join(pwd_path, 'output/logs/train')
+inferlogs_path = os.path.join(pwd_path, 'output/logs/infer')
+models_path = os.path.join(pwd_path, 'output/models')
 # Training data path.
 train_path = os.path.join(data_dir, 'train.txt')
 # Validation data path.
@@ -30,9 +32,13 @@ arch = 'seq2seq'
 # config
 src_vocab_path = os.path.join(data_dir, 'vocab_source.txt')
 trg_vocab_path = os.path.join(data_dir, 'vocab_target.txt')
-model_path = os.path.join(output_dir, 'model_batch_size-{}_dropout-{}_epochs-{}.pth'.format(batch_size, dropout, epochs))
-trainlog_path = os.path.join(output_dir, 'batch_size-{}_dropout-{}_epochs-{}.log'.format(batch_size, dropout, epochs))
-inferlog_path = os.path.join(pwd_path, 'output/infer/batch_size-{}_dropout-{}_epochs-{}/batch_size-{}_dropout-{}_epochs-{}.log'.format(batch_size, dropout, epochs, batch_size, dropout, epochs))
+model_path = os.path.join(models_path, 'batch_size-{}_dropout-{}_epochs-{}.pth'.format(batch_size, dropout, epochs))
+trainlog_path = os.path.join(trainlogs_path, 'batch_size-{}_dropout-{}_epochs-{}.log'.format(batch_size, dropout, epochs))
+inferlog_path = os.path.join(inferlogs_path, 'batch_size-{}_dropout-{}_epochs-{}.log'.format(batch_size, dropout, epochs))
 
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+if not os.path.exists(trainlogs_path):
+    os.makedirs(trainlogs_path)
+if not os.path.exists(inferlogs_path):
+    os.makedirs(inferlogs_path)
+if not os.path.exists(models_path):
+    os.makedirs(models_path)

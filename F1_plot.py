59e984223d4df65be4b3123bca700253c5e544e7
loong <lizelongdd@sina.com>
2022-06-15 00:23:59 +0800

    optimize

--- F1_plot.py
+++ F1_plot.py
@@ -9,16 +9,16 @@ fig = plt.figure(figsize=(10, 30), dpi=300)
 ax = fig.add_subplot(311,projection='3d')
 ax1 = fig.add_subplot(312,projection='3d')
 ax2 = fig.add_subplot(313,projection='3d')
-x = np.array([0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5])
+x = np.array([0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95])
 y = np.array([16, 32, 64, 128, 256, 512])
 dropout, batchsize = np.meshgrid(x, y)
 F1_path = os.path.join(results_path, 'F1.txt')
 with open(F1_path,'r') as f:
     datas = f.readlines()
-F1 = np.zeros((6, 10), dtype=float)
+F1 = np.zeros((6, 19), dtype=float)
 k = 0
 for i in range(6):
-    for j in range(10):
+    for j in range(19):
         F1[i, j] = datas[k]
         k+=1
 print('MAX is')

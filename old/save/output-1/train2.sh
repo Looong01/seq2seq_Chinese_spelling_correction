7ad06fc4e49193da5f5266119503d4c408e0d0c2
loong <lizelongdd@sina.com>
2022-06-15 00:16:37 +0800

    version2

--- old/save/output-1/train2.sh
+++
@@ -1,7 +0,0 @@
-python train.py batch_size-256_dropout-0dot25_epochs-200.py
-
-# 实验2
-python train.py batch_size-32_dropout-0dot5_epochs-200.py
-python train.py batch_size-64_dropout-0dot5_epochs-200.py
-python train.py batch_size-128_dropout-0dot5_epochs-200.py
-python train.py batch_size-256_dropout-0dot5_epochs-200.py
--- old/train2.sh
+++
@@ -1,7 +0,0 @@
-python train.py batch_size-64_dropout-0dot3_epochs-5000.py
-python train.py batch_size-128_dropout-0dot3_epochs-5000.py
-
-# 实验5
-python train.py batch_size-32_dropout-0dot2_epochs-5000.py
-python train.py batch_size-64_dropout-0dot2_epochs-5000.py
-python train.py batch_size-128_dropout-0dot2_epochs-5000.py
